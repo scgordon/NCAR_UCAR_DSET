4b29e279448d2c2880db649ab94c3b9b6b63b185
scgordon <scgordon@hdfgroup.org>
2015-12-15 11:25:08 -0700

    Rebuild of directories
     New understanding of dialectMODS path for concept Rights was added.
    EOL collection cleaned of empty record (was something I accidentally
    created.)
    MODS records added.
    
    Transforms for MODS rebuilt, JSON for EOL and IIS rebuilt both
    spreadsheets rebuilt.
    
    Next step altering the spreadsheets to be accurate.

--- Scripts/jsonCreator.sh
+++ Scripts/jsonCreator.sh
@@ -1,31 +1,31 @@
-/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh CISL ISO-1 DCITE
-/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh CISL ISO-1 RDA-CISL
-/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh CISL DCITE DCITE
-/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh CISL DCITE RDA-CISL
+#/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh CISL ISO-1 DCITE
+#/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh CISL ISO-1 RDA-CISL
+#/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh CISL DCITE DCITE
+#/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh CISL DCITE RDA-CISL
 
 /Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh EOL DCITE ISO
 /Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh EOL ISO-1 ISO
 
-/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh HAO DCITE DCITE
-/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh HAO ISO-1 DCITE
+#/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh HAO DCITE DCITE
+#/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh HAO ISO-1 DCITE
 
 /Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh IIS DCITE MODS
 /Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh IIS ISO-1 MODS
 
-/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh MMM DCITE DCITE
-/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh MMM ISO-1 DCITE
+#/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh MMM DCITE DCITE
+#/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh MMM ISO-1 DCITE
 
-/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh RAL DCITE netCDF
-/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh RAL ISO-1 netCDF
+#/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh RAL DCITE netCDF
+#/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh RAL ISO-1 netCDF
 
-/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh Unidata DCITE ISO
-/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh Unidata ISO-1 ISO
-/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh Unidata DCITE DCITE
-/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh Unidata ISO-1 DCITE
+#/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh Unidata DCITE ISO
+#/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh Unidata ISO-1 ISO
+#/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh Unidata DCITE DCITE
+#/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh Unidata ISO-1 DCITE
 
-/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh DCERC DCITE ISO
-/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh DCERC ISO-1 ISO
-/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh DCERC DCITE ISO-1
-/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh DCERC ISO-1 ISO-1
+#/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh DCERC DCITE ISO
+#/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh DCERC ISO-1 ISO
+#/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh DCERC DCITE ISO-1
+#/Users/scgordon/NCAR_UCAR_DSET/Scripts/runTransform.sh DCERC ISO-1 ISO-1
 
 
